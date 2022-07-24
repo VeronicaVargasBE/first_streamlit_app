cf9d58a2b68af08796e4a6eead6c10a6b55dd609
Veronica Vargas <105793904+VeronicaVargasBE@users.noreply.github.com>
2022-07-24 19:56:04 +0200

    Update streamlit_app.py
    added header
    added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 
 streamlit.title('My Parent New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Hard-Boiled Free-Range Egg')
