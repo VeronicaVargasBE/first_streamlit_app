8eb440cb5213c9c439afe8d835761a83c0adbc44
Veronica Vargas <veronicaelizabeth.vargassalas@bearingpoint.com>
2022-07-25 09:44:35 +0200

    added fruit table

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas as pd
 
 streamlit.title('My Parent New Healthy Diner')
 streamlit.header('Breakfast Menu')
@@ -8,3 +9,6 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
