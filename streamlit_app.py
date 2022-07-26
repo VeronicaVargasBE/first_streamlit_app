fa3b0bc8624a44ba0f067422902133ef9caddef2
Veronica Vargas <veronicaelizabeth.vargassalas@bearingpoint.com>
2022-07-26 17:30:46 +0200

    query snowflake data

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,10 +37,10 @@ fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
-# Snowflake parameters
+# Query with Snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
+streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
