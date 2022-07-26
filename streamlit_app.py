2eb50e67b1de734a14d340d4968ca2d5ed45e047
Veronica Vargas <veronicaelizabeth.vargassalas@bearingpoint.com>
2022-07-26 17:26:24 +0200

    added snowflake connector lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,3 +36,11 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
+
+# Snowflake parameters
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
