95ecd00e6041a045ce3b390ecc655988ba35b8be
Veronica Vargas <veronicaelizabeth.vargassalas@bearingpoint.com>
2022-07-26 18:16:25 +0200

    add changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,12 +45,12 @@ my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
-# # Allow the end user to add a fruit to the list
-# add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+# Allow the end user to add a fruit to the list
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 
-# if add_my_fruit != "":
-#     my_cur.execute(
-#     "INSERT INTO fruit_load_list(FRUIT_NAME) VALUES ('"+
-#     add_my_fruit+
-#     "')")
-#     streamlit.write('Thanks for adding ', add_my_fruit)
+if add_my_fruit != "" and add_my_fruit != None:
+    my_cur.execute(
+    "INSERT INTO fruit_load_list(FRUIT_NAME) VALUES ('"+
+    add_my_fruit+
+    "')")
+    streamlit.write('Thanks for adding ', add_my_fruit)
