6d0a6a6b4d2c2db00309c505b25b4f0e55613d8e
Veronica Vargas <veronicaelizabeth.vargassalas@bearingpoint.com>
2022-07-26 17:44:04 +0200

    inserting value to fruit table

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,13 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
+
+# Allow the end user to add a fruit to the list
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+streamlit.write('The user entered ', add_my_fruit)
+query_start = "INSERT INTO from fruit_load_list (FRUIT_NAME) VALUES("
+query_end = ");"
+my_cur.execute(query_start+add_my_fruit+query_end)
+my_data_row = my_cur.fetchall()
+streamlit.header("The New table")
+streamlit.dataframe(my_data_row)
