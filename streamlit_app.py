6da4cf5da2b736c22988b487d6ae302006a08aa7
Veronica Vargas <veronicaelizabeth.vargassalas@bearingpoint.com>
2022-07-26 18:08:27 +0200

    solve error

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ streamlit.dataframe(my_data_row)
 # Allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 
-if add_my_fruit != "" and add_my_fruit != None:
+if add_my_fruit != "":
     my_cur.execute(
     "INSERT INTO fruit_load_list(FRUIT_NAME) VALUES ('"+
     add_my_fruit+
