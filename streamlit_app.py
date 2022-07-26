6f5ae421e53b283034bf34e6935128d03fd6aca1
Veronica Vargas <veronicaelizabeth.vargassalas@bearingpoint.com>
2022-07-26 14:58:27 +0200

    added fruityvice choice input

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,9 @@ streamlit.dataframe(fruits_to_show)
 
 # New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit = "kiwi"
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit)
 # streamlit.text(fruityvice_response.json()) # just writes the data to the screen
 
