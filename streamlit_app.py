5bb4acda32cc047cd1ab0397f41894cc9ef0a9e6
Veronica Vargas <veronicaelizabeth.vargassalas@bearingpoint.com>
2022-07-26 14:48:02 +0200

    added watermelon api response

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
-import streamlit
 import pandas as pd
+import requests
+import streamlit
 
 streamlit.title('My Parent New Healthy Diner')
 streamlit.header('Breakfast Menu')
@@ -21,3 +22,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+# New Section to display fruityvice api response
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
