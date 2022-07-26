bd5502a8349e77f5fe944fd8475880488752a443
Veronica Vargas <veronicaelizabeth.vargassalas@bearingpoint.com>
2022-07-26 14:50:39 +0200

    addeed fruityvice header added json get response

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,5 +24,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # New Section to display fruityvice api response
+streamlit.header("Fruityvice Fruit Advice!")
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
